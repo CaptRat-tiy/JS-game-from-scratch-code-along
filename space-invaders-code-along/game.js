f024e903defe6351b41fd3ac4363a91d13acab98
captrat <jpatricklogan@gmail.comgit config user.name>
2017-03-28 17:27:03 -0600

    Add collision edge cases

--- space-invaders-code-along/game.js
+++ space-invaders-code-along/game.js
@@ -2,7 +2,7 @@
   var Game = function(canvasId) {
     var canvas = document.getElementById(canvasId);
     var screen = canvas.getContext('2d');
-    var gameSize = { x: canvas.width, y: canvas.height }
+    var gameSize = { x: canvas.width, y: canvas.height };
 
     this.bodies = createInvaders(this).concat[new Player(this, gameSize)];
 
@@ -79,16 +79,6 @@
     }
   };
 
-  var createInvaders = function(game) {
-    var invaders = [];
-    for (var i = 0; i < 24; i++) {
-      var x = 30 + (i % 8) * 30;
-      var y = 30 + (i % 3) * 30;
-      invaders.push(new Invader(game, { x: x, y: y}));
-    }
-    return invaders;
-  };
-
 // BULLET
   var Bullet = function(center, velocity) {
     this.size = { x: 3, y: 3 };
@@ -104,12 +94,7 @@
       }
     };
 
-  var drawRect = function(screen, body) {
-    // screen.fillRect(30,30,40,40);
-    screen.fillRect(body.center.x - body.size.x / 2,
-                    body.center.y - body.size.y / 2,
-                    body.size.x, body.size.y);
-}
+
 
   var Keyboarder = function() {
     var keyState = {};
@@ -127,6 +112,35 @@
   };
 
   this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32};
+  }
+
+  var createInvaders = function(game) {
+    var invaders = [];
+    for (var i = 0; i < 24; i++) {
+      var x = 30 + (i % 8) * 30;
+      var y = 30 + (i % 3) * 30;
+      invaders.push(new Invader(game, { x: x, y: y}));
+    }
+    return invaders;
+};
+
+  // var loudSound
+  //
+  // var sound = new Audio(url)
+
+  var drawRect = function(screen, body) {
+    // screen.fillRect(30,30,40,40);
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+
+  var colliding = function(b1, b2) {
+    return !(b1 === b2)||
+    b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+    b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+    b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+    b1.center.y - b1.size.x / 2 > b2.center.y - b2.size.y / 2;
+  };
 }
 
 window.onload = function() {
