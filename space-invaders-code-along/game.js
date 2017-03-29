901d45b677b43112f0ecb6046e7d8eca6a4e534c
captrat <jpatricklogan@gmail.comgit config user.name>
2017-03-28 20:13:21 -0600

    feat (index) Complete code-along but not working

--- space-invaders-code-along/game.js
+++ space-invaders-code-along/game.js
@@ -1,12 +1,18 @@
-;(function() {
+;
+(function() {
   var Game = function(canvasId) {
     var canvas = document.getElementById(canvasId);
     var screen = canvas.getContext('2d');
-    var gameSize = { x: canvas.width, y: canvas.height };
+    var gameSize = {
+      x: canvas.width,
+      y: canvas.height
+    };
 
-    this.bodies = createInvaders(this).concat[new Player(this, gameSize)];
+    this.bodies = createInvaders(this).concat(new Player(this, gameSize));
 
     var self = this;
+    loadSound("shoot.wav", function(shootSound){
+      self.shootSound = shootSound;
     var tick = function() {
       self.update();
       self.draw(screen, gameSize);
@@ -14,10 +20,19 @@
     };
 
     tick();
+    })
   };
 
   Game.prototype = {
     update: function() {
+      var bodies = this.bodies;
+      var notCollidingWithAnything = function(b1) {
+        return bodies.filter(function(b2) {
+          return colliding(b1, b2);}).length ===0;
+      };
+
+      this.bodies = this.bodies.filter(notCollidingWithAnything);
+
       for (var i = 0; i < this.bodies.length; i++) {
         this.bodies[i].update();
       }
@@ -32,6 +47,14 @@
 
     addBody: function(body) {
       this.bodies.push(body);
+    }
+
+    invadersBelow: function(invader) {
+      return this.bodies.filter(function(b) {
+        return b instanceof Invader &&
+        b.center.y > invader.center.y &&
+        b.center.x - invader.center.x < invader.size.x;
+      }).length > 0;
     }
   };
 
@@ -56,6 +79,8 @@
           { x: this.center.x, y: this.center.y - this.size.x / 2},
           { x: 0, y: -6});
         this.game.addBody(bullet);
+        this.game.shootSound.load();
+        this.game.shootSound.play();
       }
     }
   };
@@ -76,6 +101,13 @@
     }
     this.center.x += this.speedX;
     this.patrolX += this.speedX;
+
+    if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
+      var bullet = new Bullet(
+        { x: this.center.x, y: this.center.y + this.size.x / 2 },
+        { x: Math.random() - 0.5, y: 2});
+        this.game.addBody(bullet);
+      }
     }
   };
 
@@ -93,7 +125,6 @@
         this.center.y += this.velocity.y;
       }
     };
-
 
 
   var Keyboarder = function() {
@@ -122,11 +153,7 @@
       invaders.push(new Invader(game, { x: x, y: y}));
     }
     return invaders;
-};
-
-  // var loudSound
-  //
-  // var sound = new Audio(url)
+  };
 
   var drawRect = function(screen, body) {
     // screen.fillRect(30,30,40,40);
@@ -141,7 +168,16 @@
     b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
     b1.center.y - b1.size.x / 2 > b2.center.y - b2.size.y / 2;
   };
-}
+  var loadSound = function(url, callback {
+    var loaded = function(){
+      callback(sound);
+      sound.removeEventListener('canplaythrough', loaded);
+    };
+
+    var sound = new Audio(url);
+    sound.addEventListener('canplaythrough', loaded);
+    sound.load();
+  };
 
 window.onload = function() {
   new Game("screen");
